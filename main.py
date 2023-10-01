3bf1ec39fc249cd988b4f6c8cc919871e3d2eb7e
Yash Shah <shayash41@gmail.com>
2023-09-30 22:04:37 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from metaphor_python import Metaphor
+metaphor = Metaphor("6bf1ebab-62bc-4f07-9314-c933fcf16602")
+
+results = metaphor.search('hottest AI agent startups', use_autoprompt=True) 
+print()
